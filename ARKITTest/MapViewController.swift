9e01b88b5218596e4f3cfb6cef1a9cc9038f58b6
nafisar <ios.nafisa@gmail.com>
2017-12-20 20:59:08 +1100

    Added core location methods

--- ARKITTest/MapViewController.swift
+++ ARKITTest/MapViewController.swift
@@ -14,19 +14,83 @@ class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDel
     
     @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    var userLocation : CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
         mapView.setUserTrackingMode(.follow, animated: true)
         
+        //MARK: Configure location manager
+        if CLLocationManager.locationServicesEnabled() {
+            
+            locationManager.delegate = self
+            
+            locationManager.requestAlwaysAuthorization()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Core Location
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation])
+    {
+        let location  = locations.last!
+        
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        userLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+    }
+    
+    
+    //MARK: error in getting user location
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error)
+    {
+        turnOnLocation()
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+            
+        case .notDetermined:
+            manager.requestAlwaysAuthorization()
+            
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            
+        case .authorizedAlways:
+            manager.startUpdatingLocation()
+            
+        case .restricted:
+            turnOnLocation()
+            
+        case .denied:
+            turnOnLocation()
+            
+        }
+    }
+    
+    func turnOnLocation() {
+        //if time permits add an alert
+        print("turn on your location")
+    }
+    
+    
 }
 
