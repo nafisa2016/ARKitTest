80f1b6d857c5333cd26db5c2b54c1c60e46b031f
nafisar <ios.nafisa@gmail.com>
2017-12-21 15:18:51 +1100

    Added test case

--- ARKITTestUITests/ARKITTestUITests.swift
+++ ARKITTestUITests/ARKITTestUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ARKITTest
 
 class ARKITTestUITests: XCTestCase {
         
@@ -31,6 +32,15 @@ class ARKITTestUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIDevice.shared.orientation = .portrait
+        
+        let app = XCUIApplication()
+        app/*@START_MENU_TOKEN@*/.maps.containing(.other, identifier:"Hisense Arena").element/*[[".maps.containing(.other, identifier:\"Fawkner Park\").element",".maps.containing(.other, identifier:\"South Yarra Station - Stop 127\").element",".maps.containing(.other, identifier:\"South Yarra\").element",".maps.containing(.other, identifier:\"Melbourne High School\").element",".maps.containing(.other, identifier:\"Entrecôte\").element",".maps.containing(.other, identifier:\"Royal Botanic Gardens Victoria\").element",".maps.containing(.other, identifier:\"Ornamental Lake\").element",".maps.containing(.other, identifier:\"The Precinct Hotel\").element",".maps.containing(.other, identifier:\"AAMI Park\").element",".maps.containing(.other, identifier:\"Hisense Arena\").element"],[[[-1,9],[-1,8],[-1,7],[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app.navigationBars["ARKITTest.MapView"].buttons["ARView"].tap()
+        app.navigationBars["ARKITTest.ARView"].buttons["Back"].tap()
+        
+        
     }
     
 }
